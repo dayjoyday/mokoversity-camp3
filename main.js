aa20e1351a25705b8d0eebed6318b6d16d25b5a5
dayjoyday <sue012356789@yahoo.com.tw>
2013-10-19 15:20:16 +0800

    fix #5

---
+++ main.js
@@ -0,0 +1,38 @@
+var gameModule = (function () {
+
+	var timeoutvar,
+	    counter = 0;
+
+	function start() {
+  	    var canvas = document.getElementById('game'),
+	        ctx = canvas.getContext('2d'),
+
+	        ballX = Math.floor(Math.random() * 300),
+	        ballY = Math.floor(Math.random() * 500),
+	        ballR = Math.floor(Math.random() * 100);
+
+	        canvas.width = 480;
+	        canvas.height = 320;
+
+	        ctx.fillStyle = 'black';
+	        ctx.beginPath();
+	        ctx.arc( ballX, ballY, ballR, 0, Math.PI * 2, true);
+	        ctx.fill();
+
+            if(counter >= 10) {
+
+            } else{
+            	timeoutVar = setTimeout(start, 1000);
+            	counter = counter + 1;
+
+            	console.log("Counter:" + counter);
+            }
+	    }
+
+	    return {
+	    	start: start
+	    }
+
+}) ();
+
+gameModule.start();
